c48543f6769fc408ca9598c469ad5c1a8eb75eb4
Ilija Boshkov <mindcalamity@gmail.com>
2017-08-26 14:17:37 +0200

    Initial

--- main.js
+++ main.js
@@ -6,14 +6,22 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+var electronVibrancy = require('./vendor/electron-vibrancy');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    frame: false,
+    width: 400,
+    height: 400,
+    'accept-first-mouse': true,
+    'title-bar-style': 'hidden',
+    transparent: true
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -21,6 +29,37 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }))
+
+  mainWindow.on('ready-to-show', function () {
+    electronVibrancy.SetVibrancy(mainWindow, 0);
+
+    // // non-resizing vibrant view at {0,0} with size {300,300} with Material 0
+    // electronVibrancy.AddView(mainWindow, { Width: 300, Height: 300, X: 0, Y: 0, ResizeMask: 3, Material: 0 })
+
+    // //Remove a view
+    // var viewId = electronVibrancy.SetVibrancy(mainWindow, 0);
+    // electronVibrancy.RemoveView(mainWindow, viewId);
+
+    // // Add a view then update it
+    // var viewId = electronVibrancy.SetVibrancy(mainWindow, 0);
+    // electronVibrancy.UpdateView(mainWindow, { ViewId: viewId, Width: 600, Height: 600 });
+
+    // // Multipe views with different materials
+    // var viewId1 = electronVibrancy.AddView(mainWindow, { Width: 300, Height: 300, X: 0, Y: 0, ResizeMask: 3, Material: 0 })
+    // var viewId2 = electronVibrancy.AddView(mainWindow, { Width: 300, Height: 300, X: 300, Y: 0, ResizeMask: 3, Material: 2 })
+
+    // console.log(viewId1);
+    // console.log(viewId2);
+
+    // electronVibrancy.RemoveView(mainWindow,0);
+    // electronVibrancy.RemoveView(mainWindow,1);
+
+    // or
+
+    // electronVibrancy.DisableVibrancy(mainWindow);
+  });
+
+
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
--- renderer.js
+++ renderer.js
@@ -1,3 +1,81 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const $ = require('jquery');
+require('./simplescrollbars')
+
+function refreshCodeMirror(instance) {
+  instance.refresh();
+  instance.setValue(instance.getValue());
+}
+
+function getOrCreateLine(instance, lineNb) {
+  const resultLH = instance.getDoc().getLineHandle(lineNb);
+  return resultLH;
+}
+
+function setLine(instance, lineNb, content) {
+  const resultLH = getOrCreateLine(instance, lineNb);
+  resultLH.text = content && content.toString() || "";
+  refreshCodeMirror(instance);
+}
+
+function syncScroll(origin, dest) {
+  const scrollNfo = origin.getScrollInfo();
+  dest.scrollTo(scrollNfo.left, scrollNfo.top);
+}
+// Add the listener
+document.addEventListener('DOMContentLoaded', function () {
+  const actionArea = document.getElementById("actionArea");
+  const resultELement = document.getElementById("resultArea");
+
+  const codeArea = CodeMirror.fromTextArea(actionArea, {
+    lineNumbers: true,
+    mode: "javascript",
+    theme: "material",
+    scrollbarStyle: "overlay",
+    styleActiveLine: true,
+    matchBrackets: true,
+  });
+
+  const resultArea = CodeMirror.fromTextArea(resultELement, {
+    mode: "javascript",
+    theme: "material",
+    styleActiveLine: true,
+    matchBrackets: true,
+    scrollbarStyle: null,
+    readOnly: true
+  });
+
+  resultArea.getDoc().setValue("\n".repeat(10000));
+
+  codeArea.on("scroll", (instance, changeObj) => {
+    syncScroll(codeArea, resultArea);
+  });
+
+  codeArea.on("change", (instance, changeObj) => {
+    const lineNb = instance.getDoc().getCursor().line;
+    const lineContent = instance.getDoc().getLine(lineNb);
+
+    new Promise((resolve, reject) => {
+      try {
+        console.log(lineContent);
+        const isJs = lineContent.startsWith("$");
+        if (isJs) {
+          const result = eval(lineContent.substr(1));
+          console.log(result);
+          setLine(resultArea, lineNb, result);
+        }
+      } catch (err) {
+        console.log(err);
+        if (lineContent.trim().length == 0) {
+          setLine(resultArea, lineNb, "");
+        } else {
+          setLine(resultArea, lineNb, "Cannot Evaluate");
+        }
+      }
+    });
+
+    syncScroll(codeArea, resultArea);
+  });
+})
